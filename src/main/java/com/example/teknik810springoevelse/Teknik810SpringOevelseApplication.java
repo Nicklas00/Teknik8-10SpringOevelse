25b8e73ca1d5fbaeac4983e9e42a94c42f8a5f36
Nicklas00 <nick553h@stud.kea.dk>
2021-10-08 13:38:09 +0200

    first commit

---
+++ src/main/java/com/example/teknik810springoevelse/Teknik810SpringOevelseApplication.java
@@ -0,0 +1,13 @@
+package com.example.teknik810springoevelse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teknik810SpringOevelseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Teknik810SpringOevelseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teknik810springoevelse/Teknik810SpringOevelseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknik810springoevelse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teknik810SpringOevelseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
